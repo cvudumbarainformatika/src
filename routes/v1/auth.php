d8ac45b75bf941f7e0056cd14775e20e32d549cd
duro06 <andi.meka.025@gmail.com>
2024-08-20 23:22:31 +0700

    auth

--- routes/v1/auth.php
+++ routes/v1/auth.php
@@ -3,11 +3,13 @@
 use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function () {
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    // Route::post('/login', [AuthController::class, 'login'])->name('login');
     // Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     // Route::post('/refresh', [AuthController::class, 'refresh']);
