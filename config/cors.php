106c735b589f603bb1f6d379f73dabe8df5c0ed0
duro06 <andi.meka.025@gmail.com>
2024-08-20 23:12:34 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'https://apotek-setiawan-group.my.id/*'],
 
     'allowed_methods' => ['*'],
 
